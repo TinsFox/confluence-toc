db1faf37cc4a16e71d5d5dbdaade60c0ac549159
TinsFox <fox@tinsfox.com>
2024-06-25 15:49:46 +0800

    fix: remove shadow and set bg color to #fff

--- src/contents/content.ts
+++ src/contents/content.ts
@@ -15,12 +15,12 @@ style.textContent = `
   right: 35px;
   top: 50%;
   transform: translateY(-50%);
-  background-color: bisque;
+  background-color: #fff;
   width: 268px;
   z-index: 1000;
   padding: 20px;
   border-radius: 20px;
-  box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+  // box-shadow: 0 4px 6px rgba(0,0,0,0.1);
   transition: transform 0.3s ease, opacity 0.3s ease;
   max-height:80%;
   overflow: auto
